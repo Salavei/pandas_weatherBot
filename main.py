ff0b357461bf8723feff516140f95d5954f6413e
Salavei <kano.mori@mail.ru>
2021-11-14 21:53:40 +0300

    fix bug save photo, fix bug run script, fix structure code

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from prediction import get_prediction
 from weather_photo import create_image
 from time import gmtime, strftime
 
-
-API = 'YOU_TELEGRAM_API_KEY'
+API = 'YOU_API_KEY'
 bot = telebot.TeleBot(API, parse_mode=None)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.from_user.id, '🤖Привет, рад тебя видеть!👋')
+    bot.send_message(message.from_user.id, 'Чтобы узнать погоду - введи название города или страны')
 
 
 def message_send_photo(message):
@@ -20,17 +20,19 @@ def message_send_photo(message):
     img.close()
 
 
-
-admins = [815021893] #815021893 my id
+admins = [815021893]  # 815021893 my id
 @bot.message_handler(commands=['admin'])
 def helper(message):
+    user_id = message.from_user.id
+    user_name = message.from_user.username
     if message.from_user.id in admins:
-        # print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), user_id, 'воспользовался командой "admin" ')
+        print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), user_id, user_name, 'воспользовался командой "admin" ')
         bot.send_message(message.from_user.id, '🔐Аутентификация прошла успешно!🔓')
 
     else:
-        print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), 'пытался использовать команду "admin" ')
+        print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), user_id, user_name, 'пытался использовать команду "admin" ')
         bot.send_message(message.from_user.id, '❌Такой команды не существует❌')
+
 
 @bot.message_handler(content_types='text')
 def weather_reply(message):
@@ -38,13 +40,14 @@ def weather_reply(message):
     if get_prediction(city_name) == None:
         user_id = message.from_user.id
         user_name = message.from_user.username
-        print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), user_id, user_name, 'error in main.py')
+        print(strftime("%Y-%m-%d %H:%M:%S", gmtime()), user_id, user_name, message.text, 'error in main.py')
         bot.send_message(message.chat.id, f'Такого города или страны не существует🌎')
     else:
         for_image = get_prediction(city_name)
-        create_image(city_name,for_image[0],for_image[1],for_image[2])
-        bot.send_message(message.chat.id, f'Погода {city_name} | {get_prediction(city_name)[1]} ℃ ', message_send_photo(message))
+        create_image(city_name, for_image[0], for_image[1], for_image[2])
+        bot.send_message(message.chat.id, f'Погода {city_name} | {get_prediction(city_name)[1]} ℃ ',
+                         message_send_photo(message))
 
 
-
-bot.polling(none_stop=True, interval=0)
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
--- weather_photo.py
+++ weather_photo.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 
 
 def create_image(city_name, temp_min, temp, temp_max):
-    df = pd.DataFrame({'difference_weather': ['минимальная', 'сейчас', 'максимальная'], 'temp': [temp_min, temp, temp_max]})
+    df = pd.DataFrame(
+        {'difference_weather': ['минимальная', 'сейчас', 'максимальная'], 'temp': [temp_min, temp, temp_max]})
 
     plt.plot(df['difference_weather'], df['temp'])
     plt.grid(color='black', linestyle='-', linewidth=0.2)
     plt.suptitle(f'Температура {city_name}')
     plt.ylim(bottom=None)
-    return plt.savefig('image.png')
+    plt.savefig('image.png')
+    plt.show()
